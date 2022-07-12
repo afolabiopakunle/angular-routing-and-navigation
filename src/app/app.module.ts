8cac6e7cbf1b99d50fef23bde424b8d4e4da5185
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-12 23:42:58 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FollowersComponent } from './followers/followers.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PostComponent } from './post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Route[] = [
-  {path: '', component: HomeComponent }
+  {path: '', component: HomeComponent },
+  {path: 'followers', component: FollowersComponent},
+  {path: 'profile/:username', component: ProfileComponent},
+  {path: 'posts', component: PostComponent}
 ]
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, NavbarComponent, HomeComponent ],
+  declarations: [ AppComponent, NavbarComponent, HomeComponent, PostComponent, PageNotFoundComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
